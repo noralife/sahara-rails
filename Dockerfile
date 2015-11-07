ac57a3326996173fe45e49bcf048976ffbdfa075
Tatsuya Nanjo <noralife@gmail.com>
2015-11-07 03:01:24 +0000

    Fix docker-compose problem

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM ruby:2.2.0
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev
 RUN mkdir /sahara
+RUN mkdir /sahara/tmp
 WORKDIR /sahara
 ADD . /sahara
-RUN bundle install
+RUN bundle install -j4 --without development test
+EXPOSE 3000
+CMD ["bundle", "exec", "unicorn", "-c", "config/unicorn.rb"]
---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+rails_root = File.expand_path('../../', __FILE__)
+rails_env = ENV['RAILS_ENV'] || "development"
+
+worker_processes 2
+working_directory rails_root
+
+listen 3000
+pid "#{rails_root}/tmp/#{rails_env}_unicorn.pid"
+
+stderr_path "#{rails_root}/log/#{rails_env}_unicorn_error.log"
+stdout_path "#{rails_root}/log/#{rails_env}_unicorn.log"
